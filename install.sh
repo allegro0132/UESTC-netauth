facf6be05b2eec2be125c1931f505f3aa971507f
allegro0132 <34514398+allegro0132@users.noreply.github.com>
2021-03-12 15:53:50 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
 cp netauth /etc/init.d/
-mkdir /etc/init.d/netauthsrc
-cp netauthd /etc/init.d/netauthsrc
+cp netauthd /usr/local/bin
 chmod +x /etc/init.d/netauth
-chmod +x /etc/init.d/netauthsrc/netauthd
-update-rc.d netauth defaults 99
+chmod +x /usr/local/bin/netauthd
+systemctl enable netauth
 reboot
